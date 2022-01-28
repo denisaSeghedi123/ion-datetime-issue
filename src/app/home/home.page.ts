1ac29467ac95901c9623617b9fa3929854385e85
Denisa Seghedi <denisa.seghedi@corebuild.eu>
2022-01-28 14:05:54 +0200

    feat(home.page): create ion-datetime example

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
+  @ViewChild('dateOfBirthModal') dateOfBirthModal: IonModal;
+
   constructor() {}
 
+  public onDateOfBirthItemClick() {
+    this.dateOfBirthModal.present();
+  }
 }
